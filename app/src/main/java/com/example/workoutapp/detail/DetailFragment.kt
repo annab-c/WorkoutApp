2d36afab9b54e3216e972289b53902b55c0be555
Anna Balazs-Csiki <annabalazscsiki@gmail.com>
2024-01-15 19:57:03 +0100

    Bugfix date format The detail fragment now shows dates in the correct format

--- app/src/main/java/com/example/workoutapp/detail/DetailFragment.kt
+++ app/src/main/java/com/example/workoutapp/detail/DetailFragment.kt
@@ -3,29 +3,41 @@ package com.example.workoutapp.detail
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import java.text.SimpleDateFormat
+import java.util.Locale
 import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.workoutapp.R
 import com.google.android.material.textfield.TextInputEditText
+import java.util.Date
 
 class DetailFragment : Fragment(R.layout.fragment_detail) {
     private val argumentsFromOverviewScreen: DetailFragmentArgs by navArgs()
+
+    fun formatDate(timestamp: Long): String {
+        var date = Date(timestamp)
+        val format = SimpleDateFormat("dd -MM-yyyy", Locale.getDefault())
+            return format.format(date)
+    }
+
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         val workout = argumentsFromOverviewScreen.workout
 
-        val etWorkoutType = view.findViewById<TextInputEditText>(R.id.etWorkoutType)
+        var etWorkoutType = view.findViewById<TextInputEditText>(R.id.etWorkoutType)
         etWorkoutType.setText(workout.workoutType.toString())
 
-        val etWorkoutDate: TextInputEditText =
-            view.findViewById(R.id.etWorkoutDate)
-        etWorkoutDate.setText(workout.workoutDate.toString())
+        var formattedDate = formatDate(workout.workoutDate)
 
-        val etWorkoutDuration = view.findViewById<TextInputEditText>(R.id.etWorkoutDuration)
+        var etWorkoutDate = view.findViewById<TextInputEditText>(R.id.etWorkoutDate)
+        etWorkoutDate.setText(formattedDate)
+
+        var etWorkoutDuration = view.findViewById<TextInputEditText>(R.id.etWorkoutDuration)
         etWorkoutDuration.setText(workout.workoutDuration.toString())
 
         etWorkoutType.doAfterTextChanged { newType ->
